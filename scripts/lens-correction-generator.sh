ca294d89c5c0347ed3e2a14bca30f01c560915c9
Karel Fiala <fiala.karel@gmail.com>
2015-07-05 13:58:17 +0200

    added my note

--- scripts/lens-correction-generator.sh
+++ scripts/lens-correction-generator.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 for ((i=0;i<100;i++)) ; do for ((j=0;j<100;j++)); do ffmpeg -i lens.jpg -loglevel error -threads 4 -vcodec mjpeg -vf "frei0r=lenscorrection:0.5:0.5:0.$i:0.$j" lens-$i-$j.jpg ; done ; done
+
+# analizovat rozsah a snizit hranice
+# pro lamax je to cca 10-30 a 50-80
+# a opakovat 
